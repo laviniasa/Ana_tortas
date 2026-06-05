d0079805ceb32252ba49f46ba87d88675a44a545
Lavínia Sá <82878705+laviniasa@users.noreply.github.com>
2026-06-05 12:05:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+from werkzeug.utils import secure_filename
+from datetime import datetime
 import sqlite3
+import os
 
 app = Flask(__name__)
+app.secret_key = "ana_tortas_2026"
 
 @app.route("/")
 def index():
@@ -25,8 +29,25 @@ def index():
         eventos=eventos
     )
 
+
+@app.route("/login", methods=["POST"])
+def login():
+
+    usuario = request.form["usuario"]
+    senha = request.form["senha"]
+
+    if usuario == "ana" and senha == "123456":
+        session["logado"] = True
+        return redirect("/admin")
+
+    return redirect("/")
+
+
 @app.route("/admin")
 def admin():
+
+    if not session.get("logado"):
+        return redirect("/")
 
     conn = sqlite3.connect("eventos.db")
 
@@ -45,14 +66,66 @@ def admin():
         eventos=eventos
     )
 
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/")
+
+
 @app.route("/salvar-evento", methods=["POST"])
 def salvar_evento():
 
+    if not session.get("logado"):
+        return redirect("/")
+
     nome = request.form["nome"]
-    data = request.form["data"]
+
+    data_inicio = request.form["data_inicio"]
+    data_fim = request.form["data_fim"]
+
+    meses = [
+        "Janeiro", "Fevereiro", "Março", "Abril",
+        "Maio", "Junho", "Julho", "Agosto",
+        "Setembro", "Outubro", "Novembro", "Dezembro"
+    ]
+
+    inicio = datetime.strptime(data_inicio, "%Y-%m-%d")
+    fim = datetime.strptime(data_fim, "%Y-%m-%d")
+
+    if data_inicio == data_fim:
+        data = inicio.strftime("%d/%m/%Y")
+
+    elif inicio.month == fim.month and inicio.year == fim.year:
+        data = f"{inicio.day:02d} a {fim.day:02d} de {meses[inicio.month-1]}"
+
+    else:
+        data = (
+            f"{inicio.day:02d}/{inicio.month:02d} "
+            f"a "
+            f"{fim.day:02d}/{fim.month:02d}"
+        )
+
     horario = request.form["horario"]
     endereco = request.form["endereco"]
     maps_url = request.form["maps_url"]
+
+    foto = request.files["foto"]
+
+    nome_foto = ""
+
+    if foto and foto.filename != "":
+
+        nome_foto = secure_filename(foto.filename)
+
+        foto.save(
+            os.path.join(
+                "static",
+                "eventos",
+                nome_foto
+            )
+        )
 
     conn = sqlite3.connect("eventos.db")
 
@@ -64,16 +137,18 @@ def salvar_evento():
         data,
         horario,
         endereco,
-        maps_url
+        maps_url,
+        foto
     )
-    VALUES(?,?,?,?,?)
+    VALUES(?,?,?,?,?,?)
     """,
     (
         nome,
         data,
         horario,
         endereco,
-        maps_url
+        maps_url,
+        nome_foto
     ))
 
     conn.commit()
@@ -83,6 +158,9 @@ def salvar_evento():
 
 @app.route("/excluir/<int:id>")
 def excluir(id):
+
+    if not session.get("logado"):
+        return redirect("/")
 
     conn = sqlite3.connect("eventos.db")
 
